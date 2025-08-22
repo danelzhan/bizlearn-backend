0d29f3f6b7a463e64472d7bf8dce61816e6d956e
Daniel Zhang <danielz4734@gmail.com>
2025-08-21 21:32:00 -0700

    ds

--- app.py
+++ app.py
@@ -129,5 +129,6 @@ def update_user_code(email, slug):
     return "success", 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT
+    app.run(host="0.0.0.0", port=port)
